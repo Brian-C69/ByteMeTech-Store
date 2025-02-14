8159a2c5da051d753e2a97e14d3b345d837c436b
Bernard.C <bchoong1@gmail.com>
2025-02-14 23:21:30 +0800

    test

--- page/index.php
+++ page/index.php
@@ -1,1 +1,21 @@
-Hello World
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Custom Bootstrap-Like CSS</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body class="bg-light">
+
+    <div class="container text-center">
+        <h1 class="black-text bold">Welcome to ByteMeTech</h1>
+        <p class="white-text bg-dark">Your favorite place for tech!</p>
+        <a href="#" class="btn btn-primary">Shop Now</a>
+    </div>
+
+</body>
+
+</html>
